bd654db79d27f99b190f530f8d3d171af184a8e9
Mark <mark@inuksoft.com>
2018-06-13 14:16:06 -0700

    Changed project to Oregon Outback trail.  Also removed unused code.

--- GPXParserPodTest/ViewController.swift
+++ GPXParserPodTest/ViewController.swift
@@ -7,54 +7,70 @@
 //
 
 import UIKit
-import GPXXMLParser
 import MapKit
+import GPXXMLParser
 
 class ViewController: UIViewController, MKMapViewDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     
-    var parser: GPXXMLParser?
-
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
     override func viewDidLoad() {
+        var parser: GPXXMLParser?
         super.viewDidLoad()
-        
-        if let url = Bundle.main.url(forResource: "Mountain Park", withExtension: "GPX") {
+        if let url = Bundle.main.url(forResource: "Oregon Outback", withExtension: "GPX") {
             parser = GPXXMLParser(url: url)
         }
-        // Do any additional setup after loading the view, typically from a nib.
+
+        guard let gpxParser = parser else {
+            print("error creating parser")
+            abort()
+        }
         
-        let tracks = parser?.tracks
-        let track = tracks![0]
+        // extract test data from gpx
+        let tracks = gpxParser.tracks
+        let track = tracks[0]
         let tracksegs = track.trksegments
         let trackseg = tracksegs[0]
         let trackpts = trackseg.trackpoints
-        let extensions = track.extensions()
-        let trackextensions = extensions?.extensions()
-        let meta = parser?.metadata
-        let time = meta!.time()
-        let waypoints = parser?.waypoints
-        let waypoint = waypoints![0]
-        let wayptextensions = waypoint.extensions()
-
+        let meta = gpxParser.metadata
         
-        // Do any additional setup after loading the view, typically from a nib.
-        let coords1 = CLLocationCoordinate2D(latitude: 52.167894, longitude: 17.077399)
-        let coords2 = CLLocationCoordinate2D(latitude: 52.168776, longitude: 17.081326)
-        let coords3 = CLLocationCoordinate2D(latitude: 52.167921, longitude: 17.083730)
-        let testcoords:[CLLocationCoordinate2D] = [coords1,coords2,coords3]
-        
-        let testline = MKPolyline(coordinates: testcoords, count: testcoords.count)
+        var coords = [CLLocationCoordinate2D]()
+        for pt in trackpts {
+            let lat = (pt.lat as NSString).floatValue
+            let lon = (pt.lon as NSString).floatValue
+            let coord = CLLocationCoordinate2D(latitude: CLLocationDegrees(lat), longitude: CLLocationDegrees(lon))
+            coords.append(coord)
+        }
+        let polyline = MKPolyline(coordinates: coords, count: coords.count)
         
         //Add `MKPolyLine` as an overlay.
-        mapView.add(testline)
+//        mapView.add(testline)
+        mapView.add(polyline)
         
         mapView.delegate = self
         
         
         // use basecamp to get approximate center, and then use bounds to set the span.
-        mapView.centerCoordinate = coords2
-        mapView.region = MKCoordinateRegion(center: coords2, span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02))
+        let bounds = meta.bounds()
+        let minLat = (bounds!.minlat as NSString).floatValue
+        let maxLat = (bounds!.maxlat as NSString).floatValue
+        let minLon = (bounds!.minlon as NSString).floatValue
+        let maxLon = (bounds!.maxlon as NSString).floatValue
+        
+        let latDelta = abs(maxLat - minLat)
+        let lonDelta = abs(maxLon - minLon)
+        let centerLat = minLat + (latDelta/2.0)
+        let centerLon =  minLon + (lonDelta/2.0)
+        let center = CLLocationCoordinate2D(latitude: CLLocationDegrees(centerLat), longitude: CLLocationDegrees(centerLon))
+        
+        mapView.centerCoordinate = center
+        mapView.region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: CLLocationDegrees(latDelta), longitudeDelta: CLLocationDegrees(lonDelta)))
+        
+//        mapView.region = MKCoordinateRegion(center: coords2, span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02))
+
 
     }
 
